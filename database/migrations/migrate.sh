10ce4b539d4564f5730163834ccd69b2b4e51656
Jerome Vonk <lelovv@gmail.com>
2023-02-09 23:49:12 -0300

    new queries and views

--- database/migrations/migrate.sh
+++ database/migrations/migrate.sh
@@ -8,3 +8,4 @@ psql $PG_CONNECTION_STRING -f create_table_prediction.psql >/dev/null
 psql $PG_CONNECTION_STRING -f create_view_standings_A.psql >/dev/null
 psql $PG_CONNECTION_STRING -f create_view_standings_B.psql >/dev/null
 psql $PG_CONNECTION_STRING -f create_view_played_matches.psql >/dev/null
+psql $PG_CONNECTION_STRING -f create_view_statement.psql >/dev/null
