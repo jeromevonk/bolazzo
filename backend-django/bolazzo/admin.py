ddd4f5ab4fe3f84e7dd239706c17c6963110c701
Jerome Vonk <lelovv@gmail.com>
2023-02-14 21:05:23 -0300

    create-react-app

--- backend-django/bolazzo/admin.py
+++ backend-django/bolazzo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Match
 
-# Register your models here.
+class MatchAdmin(admin.ModelAdmin):
+  list_display = ('home_team', 'away_team')
+
+admin.site.register(Match, MatchAdmin)
