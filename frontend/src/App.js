b2bbbcfd5fb176a7d9fa37c597a7840d548e86a6
Jerome Vonk <lelovv@gmail.com>
2023-02-16 16:23:07 -0300

    firebase init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+// Configure Firebase.
+const firebaseConfig = require('./firebase-config.json');
+
+// Instantiate a Firebase app.
+firebase.initializeApp(firebaseConfig);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+};
 
 function App() {
+
+  const [isSignedIn, setIsSignedIn] = useState(undefined); // Local signed-in state.
+
+  console.log('render', isSignedIn);
+
+  // Listen to the Firebase Auth state and set the local state.
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      console.log('opa, ta logado');
+      setIsSignedIn(!!user);
+    });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bolazzo</h1>
+      {
+        // -------------------------------
+        // Show Firebase Auth
+        // -------------------------------
+        isSignedIn !== undefined && !isSignedIn &&
+        <div>
+          <p>Please sign-in:</p>
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </div>
+      }
+
+      {
+        // -------------------------------
+        // User already logged in
+        // -------------------------------
+        isSignedIn &&
+        <div>
+          <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+          <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+        </div>
+      }
+
     </div>
-  );
+
+  )
 }
 
 export default App;
