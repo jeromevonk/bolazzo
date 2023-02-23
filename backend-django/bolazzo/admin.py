23ac248347c401dc68db589639b3da946ca03e65
Jerome Vonk <lelovv@gmail.com>
2023-02-23 17:08:16 -0300

    remove django

--- backend-django/bolazzo/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Match
-
-class MatchAdmin(admin.ModelAdmin):
-  list_display = ('home_team', 'away_team')
-
-admin.site.register(Match, MatchAdmin)
